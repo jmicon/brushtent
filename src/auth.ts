53596ff1a91c684b12f6874abb827305e41d41ea
Josh <unicanvas28@gmail.com>
2024-08-19 23:29:11 +1000

    connecting auth.ts to neon db instead of local

--- src/auth.ts
+++ src/auth.ts
@@ -9,10 +9,10 @@ import PostgresAdapter from "@auth/pg-adapter"
 import { Pool } from "pg"
 
 const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
